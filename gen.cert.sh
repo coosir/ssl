753b81404265014da3adfb88374edca4b7e6f96f
coosir <git@coosir.com>
2025-10-21 11:01:57 +0800

    Mostlink

--- gen.cert.sh
+++ gen.cert.sh
@@ -3,7 +3,7 @@
 if [ -z "$1" ]
 then
     echo
-    echo 'Issue a wildcard SSL certificate with Fishdrowned ROOT CA'
+    echo 'Issue a SSL certificate with Mostlink ROOT CA'
     echo
     echo 'Usage: ./gen.cert.sh <domain> [<domain2>] [<domain3>] [<domain4>] ...'
     echo '    <domain>          The domain name of your site, like "example.dev",'
@@ -39,7 +39,7 @@ openssl req -new -out "${DIR}/$1.csr.pem" \
     -reqexts SAN \
     -config <(cat ca.cnf \
         <(printf "[SAN]\nsubjectAltName=${SAN}")) \
-    -subj "/C=CN/ST=Guangdong/L=Guangzhou/O=Fishdrowned/OU=$1/CN=*.$1"
+    -subj "/C=CN/ST=Shanghai/L=Shanghai/O=Mostlink/OU=$1/CN=*.$1"
 
 # Issue certificate
 # openssl ca -batch -config ./ca.cnf -notext -in "${DIR}/$1.csr.pem" -out "${DIR}/$1.cert.pem"
